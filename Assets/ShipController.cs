fdfbdfbe2ef938634df73375bdc7f50471b73d35
Gabtwotwo <Gabtwotwo@github.com>
2020-10-04 18:03:23 -0400

    Version 0.2
    First push, with art, extremely basic movement, UI with scenes, etc.

---
+++ Assets/ShipController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+
+
+    private Vector3 fingerPosition;
+    private Rigidbody2D rb;
+    private Vector3 direction;
+    private float speed = 10.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    private void FixedUpdate()
+    {
+
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            fingerPosition = Camera.main.ScreenToWorldPoint(touch.position);
+            fingerPosition.z = 0;
+            direction = (fingerPosition - transform.position);
+            rb.velocity = new Vector2(direction.x, direction.y)*speed;
+            if(touch.phase == TouchPhase.Ended)
+            {
+                rb.velocity = Vector2.zero;
+            }
+        }
+
+        //if(rb.position.x)
+    }
+
+    //void FixedUpdate()
+    //{
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+    //    float moveVertical = Input.GetAxis("Vertical");
+    //    Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+    //    movement *= Time.deltaTime;
+    //    movement *= speed;
+
+
+
+    //    // rb2d.AddForce(movement);
+    //    transform.Translate(movement);
+
+
+    //}
+}
